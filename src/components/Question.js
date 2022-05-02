6351c96cb2b064456116ab6d2b4725ec22072fdd
Trevor Vardeman <trevortx@gmail.com>
2022-05-01 18:09:32 -0700

    commit tree a75326d093bc91cf009970108ee002d37e26d103

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,23 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
-  // add useEffect code
+  useEffect(() => {
+    setTimeout(() => {
+      if (timeRemaining > 0) {
+        setTimeRemaining(timeRemaining - 1)
+      } else {
+        setTimeRemaining(10)
+        onAnswered(false)
+      }
+    }, 1000)
+
+    return function cleanup() {
+      clearTimeout(timeRemaining)
+    }
+  })
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
